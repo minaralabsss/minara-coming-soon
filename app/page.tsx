bcc9838358d9de711d6127a95e5ada39a76ccd02
minaralabsss <minaralabs@gmail.com>
2026-07-24 19:09:56 +0300

    delete

--- app/page.tsx
+++
@@ -1,5 +0,0 @@
-import ComingSoonGate from "@/components/ComingSoonGate";
-
-export default function Home() {
-  return <ComingSoonGate />;
-}
--- components/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as Hero } from "./Hero";
-export { default as EmailForm } from "./EmailForm";
-export { default as Logo } from "./Logo";
-export { default as Navigation } from "./Navigation";
-export { default as ComingSoonGate } from "./ComingSoonGate";
-export { default as ProductSpecs } from "./ProductSpecs";
-export { default as Footer } from "./Footer";
